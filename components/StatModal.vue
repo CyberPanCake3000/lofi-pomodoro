fd69f0a7513611667074bd00d4ec11c14737d41d
egatapova <ergatapovaer@gmail.com>
2024-08-31 00:20:33 +0400

    new stat modal window

--- components/StatModal.vue
+++ components/StatModal.vue
@@ -1,9 +1,16 @@
 <template>
-  <transition name="modal">
-    <div v-if="show" class="modal-overlay" @click="$emit('close')">
-      <div class="modal-content" @click.stop>
-        <h2>Statistics</h2>
-        <button @click="$emit('close')">Close</button>
+  <transition name="modal fade" tabindex="-1">
+    <div v-if="show" class="modal-overlay" @click="closeModal">
+      <div class="modal-dialog" @click.stop>
+        <div class='modal-content p-4'>
+          <div class="modal-header mb-3">
+            <h5 class="modal-title">Statistics</h5>
+            <button type="button" class="btn-close ms-auto" @click="closeModal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            We are working on it!
+          </div>
+        </div>
       </div>
     </div>
   </transition>
@@ -15,6 +22,15 @@ export default {
   props: {
     show: Boolean,
   },
+  emits: ['close'],
+  setup(props, { emit }) {
+    const closeModal = () => {
+      localSettings.value = {...props.initialSettings};
+      emit('close');
+    };
+
+    return { closeModal };
+  }
 }
 </script>
 
@@ -34,16 +50,22 @@ export default {
 
 .modal-content {
   background-color: #fff;
-  padding: 20px;
-  border-radius: 8px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
 }
 
-.modal-enter-active, .modal-leave-active {
+.modal-enter-active,
+.modal-leave-active {
   transition: opacity 0.3s;
 }
 
-.modal-enter-from, .modal-leave-to {
+.modal-enter-from,
+.modal-leave-to {
   opacity: 0;
+}
+
+.modal-dialog {
+  margin: 1.75rem auto;
+  min-width: 325px;
+  max-width: 350px;
 }
 </style>
