f256107d55f32767b8767d2375ae3b72d910250f
egatapova <ergatapovaer@gmail.com>
2024-09-11 15:10:15 +0400

    added signature on the page

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="centered-layout">
+  <div class="centered-layout d-flex flex-column">
     <NuxtLayout>
       <NuxtPage />
+      <div class='fixed-bottom d-flex justify-content-center'>
+        <span class='p-3'>*•.¸♡made by <a href='https://github.com/CyberPanCake3000'>Erzhena</a>♡¸.•*</span>
+      </div>
     </NuxtLayout>
   </div>
 </template>
